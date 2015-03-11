d08901b60a8a085cca0a18f43657b1c18153e7df
Adam <ovadia.adam@gmail.com>
2015-03-11 18:14:01 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ include device/lge/gproj-common/BoardConfigCommon.mk
 TARGET_SPECIFIC_HEADER_PATH := device/lge/e975/include
 
 BOARD_KERNEL_CMDLINE := vmalloc=600M console=null lpj=67677 user_debug=31 msm_rtb.filter=0x0 ehci-hcd.park=3 coresight-etm.boot_enable=0 androidboot.hardware=geehrc
-TARGET_KERNEL_CONFIG := cyanogenmod_gee_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_e975_defconfig
 
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
